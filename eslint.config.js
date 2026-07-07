1c7885005b3941ec561c6321fdaf5d66feba33e8
rachitajmera19 <rachitajmera19@gmail.com>
2026-07-07 21:27:21 +0530

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+        fetch: "readonly",
+        alert: "readonly",
+        SpeechSynthesisUtterance: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "semi": ["error", "always"],
+      "quotes": ["error", "single", { "avoidEscape": true }]
+    }
+  }
+];
